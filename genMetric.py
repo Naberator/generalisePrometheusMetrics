209efa43cf5a4ea4215b37fc4ef6ca7ca2f55dcc
ziv <ziv.rushinek@gmail.com>
2022-01-11 15:53:09 +0200

    fixed q break

--- genMetric.py
+++ genMetric.py
@@ -9,13 +9,26 @@ toReplaceValues = {
     "envName": "$envName"
 }
 
+def replaceRemoveAttributes(metric):
+    for attr in toRemoveAttribute:
+        metric = re.sub(r'{0}=\".*?\",?\s?'.format(attr), "", metric)
+        
+    return metric;
+
+def replaceRemoveAttributes(metric):
+    for val in toReplaceValues.keys():
+        metric = re.sub(r'({0}=)\".*?(\"[,\s]?)'.format(val), r'\1"~{0}\2'.format(toReplaceValues[val]), metric)
+        
+    return metric;
+
 inp = ""
 while inp != "q":
     inp = input("[enter a metric to generify, 'q' to exit]\n")
-    for attr in toRemoveAttribute:
-        inp = re.sub(r'{0}=\".*?\",?\s?'.format(attr), "", inp)
-    for val in toReplaceValues.keys():
-        inp = re.sub(r'({0}=)\".*?(\"[,\s]?)'.format(val), r'\1"~{0}\2'.format(toReplaceValues[val]), inp)
+    if (inp == "q"):
+        break;
+        
+    inp = replaceRemoveAttributes(inp);
+    inp = replaceRemoveAttributes(inp);
 
     subprocess.run("pbcopy", universal_newlines=True, input=inp)
     print("[generified metric]: (Copied to clipboard!)\n", inp)
